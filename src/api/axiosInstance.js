0ccb2b8fd2b9f63b06df413408f93316bdb50aff
Teegala kavya <kavyateegala2006@gmail.com>
2026-06-27 20:00:28 +0530

    Fixed auth token handling

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,37 +3,37 @@ import axios from 'axios'
 let onSessionExpired = null
 
 export const registerSessionExpiredHandler = (handler) => {
-onSessionExpired = handler
+  onSessionExpired = handler
 }
 
 const axiosInstance = axios.create({
-baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 axiosInstance.interceptors.request.use((config) => {
-const token = localStorage.getItem('aimock_token')
+  const token = localStorage.getItem('aimock_token')
 
-if (token) {
-config.headers.Authorization = `Bearer ${token}`
-}
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
 
-return config
+  return config
 })
 
 axiosInstance.interceptors.response.use(
-(response) => response,
-(error) => {
-if (error?.response?.status === 401) {
-if (onSessionExpired) {
-onSessionExpired()
-}
-}
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      if (onSessionExpired) {
+        onSessionExpired()
+      }
+    }
 
-```
-return Promise.reject(error)
-```
-
-}
+    return Promise.reject(error)
+  }
 )
 
 export default axiosInstance
